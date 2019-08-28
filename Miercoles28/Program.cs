5a462046cf8c81254a2238c2ce81f641186ac35a
unknown <Sala300@TUNDFPCGE10F372.FormacionSena.RED>
2019-08-28 17:21:35 -0500

    Initial commit

---
+++ Miercoles28/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Miercoles28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            {
+                Pantalla pan = new Pantalla();
+                pan.Boton = "encendido";
+                pan.Pulgadas = 28;
+                pan.Color = "negro";
+                pan.Dimenciones = 28;
+                pan.Marca = "dell";
+                Console.WriteLine($"Boton = {pan.Boton}");
+                Console.WriteLine($"Pulgadas = {pan.Pulgadas}");
+                Console.WriteLine($"Color = {pan.Color}");
+                Console.WriteLine($"Dimenciones = {pan.Dimenciones}");
+                Console.WriteLine($"Marca = {pan.Marca}"); 
+                pan.IngresarPantalla();
+                pan.Imprimir();
+                pan.Modificar("SAMSUNG", 28, "GRIS");
+                pan.Imprimir();
+
+
+            }
+
+        }
+    }
+}
---
+++ Miercoles28/pantalla.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Miercoles28
+{
+    class Pantalla
+    {
+        public string Boton { get; set; }
+        public int Pulgadas { get; set; }
+        public string Color { get; set; }
+        public int Dimenciones { get; set; }
+        public string Marca { get; set; }
+        public Pantalla()
+        {
+
+        }
+        public Pantalla(string boton, int pulgadas, string color)
+
+        {
+            Boton = boton;
+            Pulgadas = pulgadas;
+            Color = color;
+
+        }
+        public Pantalla(int dimenciones, string marca)
+        {
+            Dimenciones = dimenciones;
+            Marca = marca;
+        }
+        #region Metodos
+        public void IngresarPantalla()
+        {
+            Console.WriteLine("ingrese Marca");
+            Marca = Console.ReadLine();
+            Console.WriteLine("ingrese las dimenciones");
+            Dimenciones = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese color");
+            Color = Console.ReadLine();
+        }
+
+        public void Imprimir()
+        {
+            Console.WriteLine($"La marca de la pantalla es:{Marca}");
+            Console.WriteLine($"La Dimencion de la pantalla es:{Dimenciones}");
+            Console.WriteLine($"El Color de la pantalla es:{Color}");
+        }
+
+        public void Modificar(string marca, int pulgadas, string color)
+        {
+            Marca = marca;
+            Pulgadas = pulgadas;
+            Color = color;
+        }
+          #endregion
+    }
+}
